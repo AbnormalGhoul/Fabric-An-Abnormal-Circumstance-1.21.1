1151fb71792c8e4a0bd19717220e006ada87464a
Anormal <itay.sh.mail@gmail.com>
2025-05-20 14:48:30 -0700

    Initial Commit

---
+++ src/main/java/net/abnormal/anabnormalcircumstance/AnAbnormalCircumstance.java
@@ -0,0 +1,16 @@
+package net.abnormal.anabnormalcircumstance;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnAbnormalCircumstance implements ModInitializer {
+	public static final String MOD_ID = "anabnormalcircumstance";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/abnormal/anabnormalcircumstance/AnAbnormalCircumstanceClient.java
@@ -0,0 +1,10 @@
+package net.abnormal.anabnormalcircumstance;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnAbnormalCircumstanceClient implements ClientModInitializer {
+        @Override
+        public void onInitializeClient() {
+
+        }
+}
